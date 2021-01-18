00e349c2c8f991cfaeb97db8031c11d8e75b8787
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-01-18 12:23:47 +0000

    added missing functions

---
+++ dot_config/private_fish/functions/abspath.fish
@@ -0,0 +1,6 @@
+function abspath -d 'Calculates the absolute path for the given path'
+    set -l cwd ''
+    set -l curr (pwd)
+    cd $argv[1]; and set cwd (pwd); and cd $curr
+    echo $cwd
+    end
