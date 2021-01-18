9ee44ccb8134d61c612705900ca71205b32ad096
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-01-18 14:51:42 +0000

    BANG BANG!!

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,6 +9,10 @@ set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
 set -gx TERMINAL "x-terminal-emulator"
 
+# Key bindings for bang bang
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguements
+
 # User name in rprompt
 function fish_right_prompt -d "Write out the right prompt"
     set_color -o green
