0785cee4202cc353da1943a191d928fa4d75b149
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-01-20 18:13:50 +0000

    wsl only fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,9 +5,19 @@
 set -gx fish_greeting ""
 
 # Global variables
-set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
+set -gx EDITOR "$VISUAL"
 set -gx TERMINAL "x-terminal-emulator"
+
+# Key bindings for bang bang
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
+
+# WSL only settings
+if test -n "$WSL_DISTRO_NAME"
+    set -gx DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+    set LIBGL_ALWAYS_INDIRECT 1
+end
 
 # Key bindings for bang bang
 bind ! __history_previous_command
