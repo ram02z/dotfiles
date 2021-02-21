53f68c3f8e513d0af99ba6beadc8e13e434a81b9
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-02-21 22:35:07 +0000

    Move all env variables to env.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,24 +4,6 @@
 # Enviroment variables
 . ~/.config/fish/env.fish
 
-set -gx fish_greeting ""
-
-# Global variables
-set -gx VISUAL "nvim"
-set -gx EDITOR "$VISUAL"
-set -gx TERMINAL "x-terminal-emulator"
-set -gx DISPLAY :0
-
-# Key bindings for bang bang
-bind ! __history_previous_command
-bind '$' __history_previous_command_arguments
-
-# WSL2 only settings
-if string match -rq "(?i)(?=.*microsoft)^(?:[5-9])" (uname -r) 
-    set -gx DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
-    set LIBGL_ALWAYS_INDIRECT 1
-end
-
 # Key bindings for bang bang
 bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
