1448f613dd4319c730727e6c1b8643829c6d93d0
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-02-21 15:27:49 +0000

    Add SSH agent manager

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,6 +4,14 @@
 # Enviroment variables
 . ~/.config/fish/env.fish
 
+# SSH Agent
+if not pgrep -f ssh-agent > /dev/null
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+
 set -gx fish_greeting ""
 
 # Global variables
