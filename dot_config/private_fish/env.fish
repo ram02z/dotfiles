4d5b2eea70146228fc44d8cbffe265cec70ba664
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-02-21 22:35:07 +0000

    Move all env variables to env.fish

--- dot_config/private_fish/env.fish
+++ dot_config/private_fish/env.fish
@@ -1,3 +1,25 @@
+#
+# Global Variables
+#
+set -gx fish_greeting ""
+set -gx VISUAL "nvim"
+set -gx TERMINAL "x-terminal-emulator"
+set -gx DISPLAY :0
+
+# WSL2 only settings
+if string match -rq "(?i)(?=.*microsoft)^(?:[5-9])" (uname -r)
+    set -gx DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+    set -gx LIBGL_ALWAYS_INDIRECT 1
+end
+
+# Wayland only settings
+if test -n "$WAYLAND_DISPLAY"
+    set -gx MOZ_ENABLE_WAYLAND 1
+end
+
+#
+# PATHS
+#
 set -gx PATH /bin
 append-to-path /usr/bin
 append-to-path ~/bin
