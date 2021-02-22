5e9b573fa2b519cd0763406b4f1fbec27dbe33b4
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-02-22 13:50:01 +0000

    Kill ssh agent on exit

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -14,6 +14,16 @@ function fish_right_prompt -d "Write out the right prompt"
     echo -n "$USER"
 end
 
+# Logout function
+function on_exit --on-event fish_exit
+   if test -n "$SSH_AGENT_PID"
+       ssh-add -D
+       pkill ssh-agent
+       unset SSH_AGENT_PID
+       unset SSH_AUTH_SOCK
+    end
+end
+
 # Starship Prompt
 if type -q starship
     starship init fish | source
