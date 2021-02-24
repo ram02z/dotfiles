d9e6747ae7accf3b1cb02e6f4e7076b762357baa
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-02-24 22:39:13 +0000

    Add rofi config and theme

--- dot_config/private_fish/functions/init_source.fish
+++ dot_config/private_fish/functions/init_source.fish
@@ -1,9 +1,7 @@
 function init_source -d "Initalizes scripts"
-    if begin; test (count $argv) -gt 0; and test -d $argv[1]; end
-        for cmd in $argv
-            if type -q $cmd
-                $cmd init fish | source
-            end
+    for cmd in $argv
+        if type -q $cmd
+            $cmd init fish | source
         end
     end
 end
