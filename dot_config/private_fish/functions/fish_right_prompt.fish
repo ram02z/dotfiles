ba8dcf4c2c151009f9c3a2524b143a9aaad88544
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-02-27 00:24:29 +0000

    Add TODO

--- dot_config/private_fish/functions/fish_right_prompt.fish
+++ dot_config/private_fish/functions/fish_right_prompt.fish
@@ -1,4 +1,5 @@
 # user name in right prompt
+# TODO: Make this display git status async unless starship gets async 
 function fish_right_prompt -d "Username in green"
     set_color -o green
     echo -n (whoami)
