17dca79210d50e4734ab872a1137f42e95fef013
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-04-22 00:08:16 +0100

    Nnn fixes
    - Add NNN as command for vim plugin
    - remove x flag which has no effect on wayland

--- dot_config/private_fish/conf.d/n.fish
+++ dot_config/private_fish/conf.d/n.fish
@@ -5,7 +5,7 @@ set -x NNN_PLUG "$NNN_PLUG_UTIL;$NNN_PLUG_STD"
 # set -x NNN_COLORS 'b7d49f2e;5555'
 set -x NNN_FCOLORS 'c1e2b72e006033f7c6d6abc4'
 set -x NNN_TRASH 2
-set -x NNN_OPTS "dcHxr"
+set -x NNN_OPTS "dcHr"
 set -x NNN_FIFO '/tmp/nnn.fifo'
 set -x NNN_OPENER "$HOME/.config/nnn/plugins/nuke"
 set -x GUI 1
