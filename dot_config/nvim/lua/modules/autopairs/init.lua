6e4afd3d2ea86a344e218c7900ba508d6957b5a1
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-04-26 15:42:07 +0100

    Update pears config based on fix

--- dot_config/nvim/lua/modules/autopairs/init.lua
+++ dot_config/nvim/lua/modules/autopairs/init.lua
@@ -6,7 +6,7 @@ pears.setup(function(conf)
     conf.remove_pair_on_outer_backspace(false)
     conf.on_enter(function(pear_handle)
         if vim.fn.pumvisible() == 1  and vim.fn.complete_info().selected ~= -1 then
-            vim.api.nvim_feedkeys(vim.fn["compe#confirm"]("<CR>"), "n", true)
+            return vim.fn["compe#confirm"]("<CR>")
         else
             pear_handle()
         end
