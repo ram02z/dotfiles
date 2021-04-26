46dbacb8c0bdcf63a364bc71cdaa7fd350152207
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-04-26 02:18:05 +0100

    Re-enable pears.nvim
    - Updated lua config based on answer from https://github.com/steelsojka/pears.nvim/issues/12

--- dot_config/nvim/lua/modules/autopairs/init.lua
+++ dot_config/nvim/lua/modules/autopairs/init.lua
@@ -1,30 +1,31 @@
 -- pears.nvim config
--- local pears = require('pears')
---
--- pears.setup(function(conf)
-    -- conf.on_insert_enter(function(exec)
-        -- if vim.fn.pumvisible() == 1 then
-            -- vim.fn["compe#confirm"]("<CR>")
-        -- else
-            -- exec()
-        -- end
-    -- end)
--- end)
+local pears = require('pears')
+
+pears.setup(function(conf)
+    conf.preset "tag_matching"
+    conf.on_enter(function(pear_handle)
+        if vim.fn.pumvisible() == 1  and vim.fn.complete_info().selected ~= -1 then
+            vim.api.nvim_feedkeys(vim.fn["compe#confirm"]("<CR>"), "n", true)
+        else
+            pear_handle()
+        end
+    end)
+end)
 
 -- nvim-autopairs config
 
-local autopairs = require'nvim-autopairs'
-
-
-autopairs.setup({
-    check_ts = true,
-    check_line_pair = false,
-})
-
-local M = {}
-
-M.confirm = function()
-  autopairs.check_break_line_char()
-end
-
-return M
+-- local autopairs = require'nvim-autopairs'
+--
+--
+-- autopairs.setup({
+    -- check_ts = true,
+    -- check_line_pair = false,
+-- })
+--
+-- local M = {}
+--
+-- M.confirm = function()
+  -- autopairs.check_break_line_char()
+-- end
+--
+-- return M
