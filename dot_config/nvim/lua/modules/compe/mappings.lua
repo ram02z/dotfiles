b1181bcc710af81d46013b8d0c2072cfecfd18ed
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-04-26 21:36:57 +0100

    Update compe config and mappings

--- dot_config/nvim/lua/modules/compe/mappings.lua
+++ dot_config/nvim/lua/modules/compe/mappings.lua
@@ -3,10 +3,10 @@ local opts = {'override', 'silent', 'expr'}
 
 vimp.inoremap(opts, '<CR>', 'compe#confirm(\'<CR>\')')
 
-vimp.inoremap(opts, '<C-Space>', 'compe#complete()')
+vimp.inoremap(opts, '<C-Space>', 'pumvisible() ? compe#close() : compe#complete()')
 
-vimp.inoremap(opts, '<C-e>', 'compe#close(\'<C-e>\')')
+vimp.inoremap(opts, '<TAB>', 'v:lua.tab_complete()')
+vimp.snoremap(opts, '<TAB>', 'v:lua.tab_complete()')
 
-vimp.inoremap(opts, '<TAB>', 'pumvisible() ? "\\<C-n>" : "\\<TAB>"')
-
-vimp.inoremap(opts, '<S-TAB>', 'pumvisible() ? "\\<C-p>" : "\\<C-d>"')
+vimp.inoremap(opts, '<S-TAB>', 'v:lua.s_tab_complete()')
+vimp.snoremap(opts, '<S-TAB>', 'v:lua.s_tab_complete()')
