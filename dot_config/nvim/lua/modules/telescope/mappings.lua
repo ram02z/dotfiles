867d89e9baa1661c03bd77a3c0057207e55c9baf
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-04-26 01:09:51 +0100

    Add nvim-reload

--- dot_config/nvim/lua/modules/telescope/mappings.lua
+++ dot_config/nvim/lua/modules/telescope/mappings.lua
@@ -1,28 +1,29 @@
 local vimp = require'vimp'
 local tscope = require'modules.telescope'
+local opts = {'override', 'silent'}
 
 -- Mappings
 
 -- View help pages
-vimp.nnoremap({'silent'}, '<Leader>h', tscope.help_pages)
+vimp.nnoremap(opts, '<Leader>h', tscope.help_pages)
 
 -- View mappings
-vimp.nnoremap({'silent'}, '<Leader>?', tscope.key_binds)
+vimp.nnoremap(opts, '<Leader>?', tscope.key_binds)
 
 -- Project files via git fallback to cwd
-vimp.nnoremap({'silent'}, '<Leader>pg', tscope.project_files)
+vimp.nnoremap(opts, '<Leader>pg', tscope.project_files)
 
 -- Live grep cwd
-vimp.nnoremap({'silent'}, '<Leader>pl', tscope.grep_cwd)
+vimp.nnoremap(opts, '<Leader>pl', tscope.grep_cwd)
 
 -- Live grep open files
-vimp.nnoremap({'silent'}, '<Leader>po', tscope.grep_buffers)
+vimp.nnoremap(opts, '<Leader>po', tscope.grep_buffers)
 
 -- Current buffer search
-vimp.nnoremap({'silent'}, '<Leader>pb', tscope.fz_buffer)
+vimp.nnoremap(opts, '<Leader>pb', tscope.fz_buffer)
 
 -- List git commits
-vimp.nnoremap({'silent'}, '<Leader>pc', tscope.git_commits)
+vimp.nnoremap(opts, '<Leader>pc', tscope.git_commits)
 
 -- Treesitter picker
-vimp.nnoremap({'silent'}, '<Leader>pt', tscope.treesitter)
+vimp.nnoremap(opts, '<Leader>pt', tscope.treesitter)
