ecd53e6aaa7de1679743397e1483368decc7342f
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-01 16:33:04 +0100

    Add ts-playground

--- dot_config/nvim/lua/modules/bufferline/init.lua
+++ dot_config/nvim/lua/modules/bufferline/init.lua
@@ -5,6 +5,7 @@ bline_conf = {
         numbers = "ordinal",
         number_style = "none",
         show_buffer_close_icons = false,
+        show_close_icon = false,
         always_show_bufferline = false,
         diagnostics = "nvim_lsp",
     },
