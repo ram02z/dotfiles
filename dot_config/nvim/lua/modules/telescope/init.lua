a1847bb7034ad137373127327981877dd1e5e976
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-03 00:19:33 +0100

    Fix indent on nvim lua files 4->2
    - Changed space and tab settings
    
    - Disabled indent from treesitter
    
    - Add smartcase to search
    
    - Might start using .editorconfig

--- dot_config/nvim/lua/modules/telescope/init.lua
+++ dot_config/nvim/lua/modules/telescope/init.lua
@@ -3,28 +3,28 @@ local telescope = require('telescope')
 local previewers = require('telescope.previewers')
 
 tscope_config = {
-    defaults = {
-        layout_strategy = 'flex',
-        scroll_strategy = 'cycle',
-        file_previewer = previewers.vim_buffer_cat.new,
-        grep_previewer = previewers.vim_buffer_vimgrep.new,
-        qflist_previewer = previewers.vim_buffer_qflist.new,
-        layout_defaults = {
-          horizontal = {
-            mirror = false,
-          },
-          vertical = {
-            mirror = false,
-          },
-        },
+  defaults = {
+    layout_strategy = 'flex',
+    scroll_strategy = 'cycle',
+    file_previewer = previewers.vim_buffer_cat.new,
+    grep_previewer = previewers.vim_buffer_vimgrep.new,
+    qflist_previewer = previewers.vim_buffer_qflist.new,
+    layout_defaults = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
     },
-    extensions = {
-        fzf = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-        },
+  },
+  extensions = {
+    fzf = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+      case_mode = "smart_case",
     },
+  },
 }
 
 telescope.setup(tscope_config)
@@ -36,52 +36,52 @@ local cwd_opt = {
 }
 
 M.project_files = function()
-    local ok = pcall(require'telescope.builtin'.git_files)
-    if not ok then 
-        require'telescope.builtin'.find_files(cwd_opt) 
-    end
+  local ok = pcall(require'telescope.builtin'.git_files)
+  if not ok then 
+    require'telescope.builtin'.find_files(cwd_opt) 
+  end
 end
 
 M.git_commits = function()
-    pcall(require'telescope.builtin'.git_commits,cwd_opt)
+  pcall(require'telescope.builtin'.git_commits,cwd_opt)
 end
-    
+
 M.grep_cwd = function()
-    require'telescope.builtin'.live_grep(cwd_opt)
+  require'telescope.builtin'.live_grep(cwd_opt)
 end
 
 M.grep_buffers = function()
-    require'telescope.builtin'.live_grep({
-       grep_open_files = true, 
-    })
+  require'telescope.builtin'.live_grep({
+    grep_open_files = true, 
+  })
 end
 
 M.fz_buffer = function()
-    require'telescope.builtin'.current_buffer_fuzzy_find()
+  require'telescope.builtin'.current_buffer_fuzzy_find()
 end
 
 M.help_pages = function()
-    require'telescope.builtin'.help_tags()
+  require'telescope.builtin'.help_tags()
 end
 
 M.key_binds = function()
-    require'telescope.builtin'.keymaps()
+  require'telescope.builtin'.keymaps()
 end
 
 M.treesitter = function()
-    require'telescope.builtin'.treesitter()
+  require'telescope.builtin'.treesitter()
 end
 
 M.registers = function()
-    require'telescope.builtin'.registers()
+  require'telescope.builtin'.registers()
 end
 
 M.old_files = function()
-    require'telescope.builtin'.oldfiles()
+  require'telescope.builtin'.oldfiles()
 end
 
 M.bmarks = function()
-    require'telescope.builtin'.marks()
+  require'telescope.builtin'.marks()
 end
 
 return M
