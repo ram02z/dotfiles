523431d98afa62bb5765ba0736e5b6a05121c4f3
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-09 00:12:31 +0100

    Add custom patched font
    - Closes #11
    
    - Add nonicon nvim plugin
    
    - Add vscode icons to lsp snippet

--- dot_config/nvim/lua/modules/lspinstall/init.lua
+++ dot_config/nvim/lua/modules/lspinstall/init.lua
@@ -14,6 +14,35 @@ local function make_config()
   }
 end
 
+-- Requires codicons font
+require'vim.lsp.protocol'.CompletionItemKind = {
+      '  Text';          -- = 1
+      '  Function';      -- = 2;
+      '  Method';        -- = 3;
+      '  Constructor';   -- = 4;
+      '  Field';         -- = 5;
+      '  Variable';      -- = 6;
+      '  Class';         -- = 7;
+      '  Interface';     -- = 8;
+      '  Module';        -- = 9;
+      '  Property';      -- = 10;
+      '  Unit';          -- = 11;
+      '  Value';         -- = 12;
+      '  Enum';          -- = 13;
+      '  Keyword';       -- = 14;
+      '  Snippet';       -- = 15;
+      '  Color';         -- = 16;
+      '  File';          -- = 17;
+      '  Reference';     -- = 18;
+      '  Folder';        -- = 19;
+      '  EnumMember';    -- = 20;
+      '  Constant';      -- = 21;
+      '  Struct';        -- = 22;
+      '  Event';         -- = 23;
+      '  Operator';      -- = 24;
+      '  TypeParameter'; -- = 25;
+}
+
 -- lsp-install
 local function setup_servers()
   require'lspinstall'.setup()
