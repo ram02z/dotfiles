3e8b73b643cdc6afe05a1adcf3d79e25216e5909
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-08 17:25:03 +0100

    Improve lsp-install setup

--- dot_config/nvim/lua/modules/lspinstall/init.lua
+++ dot_config/nvim/lua/modules/lspinstall/init.lua
@@ -1,16 +1,38 @@
-local lspinstall = require'lspinstall'
+local on_attach = function(client, buffnr)
+  require'illuminate'.on_attach(client)
+end
 
-local setup_servers = function()
-  lspinstall.setup()
-  local servers = lspinstall.installed_servers()
+-- config that activates keymaps and enables snippet support
+local function make_config()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  return {
+    -- enable snippet support
+    capabilities = capabilities,
+    -- map buffer local keybindings when the language server attaches
+    on_attach = on_attach,
+  }
+end
+
+-- lsp-install
+local function setup_servers()
+  require'lspinstall'.setup()
+
+  -- get all installed servers
+  local servers = require'lspinstall'.installed_servers()
+  -- ... and add manually installed servers
+  -- table.insert(servers, "sourcekit")
+
   for _, server in pairs(servers) do
-    lspinstall[server].setup{}
+    local config = make_config()
+    require'lspconfig'[server].setup(config)
   end
 end
 
 setup_servers()
 
-lspinstall.post_install_hook = function ()
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
   setup_servers() -- reload installed servers
   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
