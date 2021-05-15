50cb02bf378ad132c03b0968168b9339a340a78a
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-15 23:07:36 +0100

    nvim: change statusline read only icon

--- dot_config/nvim/lua/modules/statusline/init.lua
+++ dot_config/nvim/lua/modules/statusline/init.lua
@@ -68,7 +68,7 @@ end
 
 local function file_readonly()
   if vim.bo.filetype == 'help' then return '' end
-  if vim.bo.readonly == true then return '' end
+  if vim.bo.readonly == true then return '' end
   return ''
 end
 
