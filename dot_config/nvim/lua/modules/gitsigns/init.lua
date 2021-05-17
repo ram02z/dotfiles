a3d593d2ffe54aa0dae04f18f6163529f3e5f52c
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-17 23:50:30 +0100

    nvim: add gitsigns config

--- dot_config/nvim/lua/modules/gitsigns/init.lua
+++ dot_config/nvim/lua/modules/gitsigns/init.lua
@@ -1,5 +1,40 @@
 local gitsigns = require'gitsigns'
 
--- Add config
+local gs_conf = {
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
+  numhl = false,
+  linehl = false,
+  keymaps = {
+    noremap = true,
+    buffer = true,
 
-gitsigns.setup()
+    ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+    ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+
+    ['n <leader>gs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['n <leader>gu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <leader>gr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <leader>gR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+    ['n <leader>gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+  },
+  watch_index = {
+    interval = 1000
+  },
+  current_line_blame = false,
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  use_decoration_api = true,
+  use_internal_diff = true,  -- If luajit is present
+}
+
+
+
+gitsigns.setup(gs_conf)
