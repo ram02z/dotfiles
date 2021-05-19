fb44077fa7cbf7c177789915b37c187e64c84624
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-19 13:30:35 +0100

    nvim: manually disable compe sources when not in use
    - Related issue: #7
        - 95ms->89ms (~6ms)

--- dot_config/nvim/lua/modules/compe/init.lua
+++ dot_config/nvim/lua/modules/compe/init.lua
@@ -11,6 +11,25 @@ vim.o.pumheight = 5
 -- Increase menu width
 vim.o.pumwidth = 25
 
+-- Manually disable sources
+-- Lazy load coming soon :)
+vim.g.loaded_compe_buffer = 1
+vim.g.loaded_compe_calc = 1
+vim.g.loaded_compe_emoji = 0
+vim.g.loaded_compe_luasnip = 0
+vim.g.loaded_compe_nvim_lsp = 1
+vim.g.loaded_compe_nvim_lua = 0
+vim.g.loaded_compe_omni = 0
+vim.g.loaded_compe_path = 1
+vim.g.loaded_compe_snippets_nvim = 0
+vim.g.loaded_compe_spell = 0
+vim.g.loaded_compe_tags = 0
+vim.g.loaded_compe_treesitter = 0
+vim.g.loaded_compe_ultisnips = 0
+vim.g.loaded_compe_vim_lsc = 0
+vim.g.loaded_compe_vim_lsp = 0
+vim.g.loaded_compe_vsnip = 1
+
 local compe = require('compe')
 
 local compe_conf = {
