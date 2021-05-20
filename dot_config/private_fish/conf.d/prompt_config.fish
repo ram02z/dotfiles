002c3c6c20461967466a799143db8e408f085914
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-20 20:10:34 +0100

    fish: refactor prompts

---
+++ dot_config/private_fish/conf.d/prompt_config.fish
@@ -0,0 +1,42 @@
+# Default appearance options. Override in config.fish if you want.
+if ! set -q prompt_dirty_indicator
+    set -g prompt_dirty_indicator "•"
+end
+
+if ! set -q prompt_prompt_symbol
+    set -g prompt_prompt_symbol "❯"
+end
+
+if ! set -q prompt_prompt_symbol_error
+    set -g prompt_prompt_symbol_error "❯"
+end
+
+if ! set -q prompt_prompt_symbol_color
+    set -g prompt_prompt_symbol_color brgreen
+end
+
+if ! set -q prompt_prompt_symbol_error_color
+    set -g prompt_prompt_symbol_error_color brred
+end
+
+# This should be set to be at least as long as prompt_dirty_indicator, due to a fish bug
+if ! set -q prompt_clean_indicator
+    set -g prompt_clean_indicator (string replace -r -a '.' ' ' $prompt_dirty_indicator)
+end
+
+if ! set -q prompt_cwd_color
+    set -g prompt_cwd_color brcyan
+end
+
+if ! set -q prompt_git_color
+    set -g prompt_git_color brmagenta
+end
+
+if ! set -q prompt_lock_dir_symbol
+    set -g prompt_lock_dir_symbol ""
+end
+
+if ! set -q prompt_dir_depth
+    set -g prompt_dir_depth 4
+end
+
--- dot_config/private_fish/functions/fish_right_prompt.fish
+++ dot_config/private_fish/functions/fish_right_prompt.fish
@@ -1,6 +1,35 @@
-# user name in right prompt
-# TODO: Make this display git status async unless starship gets async 
-function fish_right_prompt -d "Username in green"
-    set_color -o green
-    echo -n (whoami)
+if ! set -q prompt_dur_color
+    set -g prompt_dur_color bryellow
+end
+
+if ! set -q __prompt_cmd_duration
+    set -g __prompt_cmd_duration (set_color --bold $prompt_dur_color)"0ms"(set_color normal)
+end
+
+function __prompt_postexec --on-event fish_postexec
+    set --local secs (math --scale=1 $CMD_DURATION/1000 % 60)
+    set --local mins (math --scale=0 $CMD_DURATION/60000 % 60)
+    set --local hours (math --scale=0 $CMD_DURATION/3600000)
+
+    test $hours -gt 0 && set --local --append out $hours"h"
+    test $mins -gt 0 && set --local --append out $mins"m"
+    test $secs -gt 0 && set --local --append out $secs"s"
+
+    if test $CMD_DURATION -lt 1000
+       set out $CMD_DURATION"ms"
+    end
+
+    set --global __prompt_cmd_duration (set_color --bold $prompt_dur_color)"$out"(set_color normal)
+end
+
+function fish_right_prompt
+    test -n "$USER" && set --local user $USER || set --local user (whoami)
+    set_color -o brgreen
+    echo -sn $user
+    set_color normal
+
+    if test -n "$__prompt_cmd_duration"
+        echo -sn " took $__prompt_cmd_duration"
+    end
+
 end
