87b9f883649270b796b90c25322d6032b9c8c128
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-20 20:32:27 +0100

    fish: add branch symbol as config variable

--- dot_config/private_fish/conf.d/prompt_config.fish
+++ dot_config/private_fish/conf.d/prompt_config.fish
@@ -3,25 +3,34 @@ if ! set -q prompt_dirty_indicator
     set -g prompt_dirty_indicator "•"
 end
 
-if ! set -q prompt_prompt_symbol
-    set -g prompt_prompt_symbol "❯"
+if ! set -q prompt_symbol
+    set -g prompt_symbol "❯"
 end
 
-if ! set -q prompt_prompt_symbol_error
-    set -g prompt_prompt_symbol_error "❯"
-end
-
-if ! set -q prompt_prompt_symbol_color
-    set -g prompt_prompt_symbol_color brgreen
-end
-
-if ! set -q prompt_prompt_symbol_error_color
-    set -g prompt_prompt_symbol_error_color brred
+if ! set -q prompt_symbol_error
+    set -g prompt_symbol_error "❯"
 end
 
 # This should be set to be at least as long as prompt_dirty_indicator, due to a fish bug
 if ! set -q prompt_clean_indicator
     set -g prompt_clean_indicator (string replace -r -a '.' ' ' $prompt_dirty_indicator)
+end
+
+if ! set -q prompt_lock_dir_symbol
+    set -g prompt_lock_dir_symbol ""
+end
+
+
+if ! set -q prompt_branch_symbol
+    set -g prompt_branch_symbol ""
+end
+
+if ! set -q prompt_symbol_color
+    set -g prompt_symbol_color brgreen
+end
+
+if ! set -q prompt_symbol_error_color
+    set -g prompt_symbol_error_color brred
 end
 
 if ! set -q prompt_cwd_color
@@ -30,10 +39,6 @@ end
 
 if ! set -q prompt_git_color
     set -g prompt_git_color brmagenta
-end
-
-if ! set -q prompt_lock_dir_symbol
-    set -g prompt_lock_dir_symbol ""
 end
 
 if ! set -q prompt_dir_depth
