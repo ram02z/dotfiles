49c45d46b8f3d151d7d3ee26c2b2a0351801c62d
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-24 02:28:03 +0100

    fish: fix right prompt `__prompt_postexec` function

--- dot_config/private_fish/functions/fish_right_prompt.fish
+++ dot_config/private_fish/functions/fish_right_prompt.fish
@@ -7,18 +7,19 @@ if ! set -q __prompt_cmd_duration
 end
 
 function __prompt_postexec --on-event fish_postexec
-    test "$CMD_DURATION" -le 0 && set --global __prompt_cmd_duration && return
+    test $CMD_DURATION -le 0 && set --global __prompt_cmd_duration && return
+    set --local out
 
     if test $CMD_DURATION -lt 1000
-        set --local out $CMD_DURATION"ms"
+        set out $CMD_DURATION"ms"
     else
         set --local secs (math --scale=1 $CMD_DURATION/1000 % 60)
         set --local mins (math --scale=0 $CMD_DURATION/60000 % 60)
         set --local hours (math --scale=0 $CMD_DURATION/3600000)
 
-        test $hours -gt 0 && set --local --append out $hours"h"
-        test $mins -gt 0 && set --local --append out $mins"m" && set --local secs (string split . $secs)[1]
-        test $secs -gt 0 && set --local --append out $secs"s"
+        test $hours -gt 0 && set --append out $hours"h"
+        test $mins -gt 0 && set --append out $mins"m" && set --local secs (string split . $secs)[1]
+        test $secs -gt 0 && set --append out $secs"s"
     end
 
     set --global __prompt_cmd_duration (set_color --bold $prompt_dur_color)"$out"(set_color normal)
