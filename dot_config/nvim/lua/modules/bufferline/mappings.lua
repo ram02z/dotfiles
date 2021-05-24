a0941bfb5084fa7b1160873b65407477547c3d98
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-05-24 03:37:21 +0100

    nvim: change bufferwipeout function logic

--- dot_config/nvim/lua/modules/bufferline/mappings.lua
+++ dot_config/nvim/lua/modules/bufferline/mappings.lua
@@ -29,7 +29,16 @@ vimp.nnoremap(opts, '<Leader>l', ':e#<CR>')
 vimp.nnoremap(opts, "<Leader>q", function()
   local buflisted = vim.fn.getbufinfo({buflisted = 1})
   local cur_winnr, cur_bufnr = vim.fn.winnr(), vim.fn.bufnr()
-  if #buflisted < 2 then vim.cmd 'confirm qall' return end
+  if #buflisted < 2 then
+    -- Requires Dashboard
+    if vim.fn.getbufvar(cur_bufnr, '&filetype') ~= 'dashboard' then
+      vim.cmd 'silent! confirm bwipeout'
+      vim.cmd 'Dashboard'
+    else
+      vim.cmd 'confirm qall'
+    end
+    return
+  end
   for _, winid in ipairs(vim.fn.getbufinfo(cur_bufnr)[1].windows) do
     vim.cmd(string.format('%d wincmd w', vim.fn.win_id2win(winid)))
     vim.cmd(cur_bufnr == buflisted[#buflisted].bufnr and 'bp' or 'bn')
