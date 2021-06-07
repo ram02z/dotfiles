e92b54c54aa24107f099059e24ea554e59086c68
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-06-07 18:59:55 +0100

    fish: add visual line bind

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,8 @@
 function fish_mode_prompt; end
 # fish_vi_key_bindings default
 
-# Autostart functions moved to ./conf.d
+# Visual line
+bind -m visual V beginning-of-line begin-selection end-of-line force-repaint
 
 # Source init scripts
 init_source zoxide
