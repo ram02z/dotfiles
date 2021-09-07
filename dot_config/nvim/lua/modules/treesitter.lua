5dbf393ce10ebb2cf3ca6c8418a65b8ded1e8131
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-09-07 09:56:37 +0100

    refactor: move norg parser to treesitter config

--- dot_config/nvim/lua/modules/treesitter.lua
+++ dot_config/nvim/lua/modules/treesitter.lua
@@ -80,6 +80,16 @@ local ts_config = {
   },
 }
 
-require("nvim-treesitter.install").compilers = { "clang", "gcc" }
+require("nvim-treesitter.install").compilers = { "clang", "gcc", "cc" }
+
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_configs.norg = {
+  install_info = {
+    url = "https://github.com/vhyrro/tree-sitter-norg",
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main",
+  },
+}
 
 tree_sitter.setup(ts_config)
