a7fc472ba0fbaedaa25dd9752dea562b826354da
Omar Zeghouani <omarzeghouanii@gmail.com>
2022-01-24 12:16:00 +0000

    chore: remove dead code

--- dot_config/nvim/lua/modules/treesitter.lua
+++ dot_config/nvim/lua/modules/treesitter.lua
@@ -69,12 +69,19 @@ require("nvim-treesitter.install").compilers = { "clang", "gcc", "cc" }
 
 local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
 
-parser_configs.norg = {
+parser_configs.norg_meta = {
   install_info = {
-    url = "https://github.com/vhyrro/tree-sitter-norg",
-    files = { "src/parser.c", "src/scanner.cc" },
+    url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+    files = { "src/parser.c" },
     branch = "main",
   },
 }
 
+parser_configs.norg_table = {
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+}
 tree_sitter.setup(ts_config)
